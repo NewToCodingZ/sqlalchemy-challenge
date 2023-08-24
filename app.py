974542087f0d9dc53d0b620939f1b5d21ea41c44
NewToCodingZ <121516782+NewToCodingZ@users.noreply.github.com>
2023-08-23 21:00:38 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,84 @@
+import numpy as np
+import re
+import datetime as dt
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from sqlalchemy.sql import exists  
+
+from flask import Flask, jsonify
+
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the tables
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+    )
+
+
+@app.route("/api/v1.0/precipitation") #Convert query results to a dictionary using `date` as the key and `tobs` as the value
+def precipitation():
+    # Create session (link) from Python to the DB
+    session = Session(engine)
+
+    # Query Measurement
+    results = (session.query(Measurement.date, Measurement.tobs)
+                      .order_by(Measurement.date))
+    
+    # Create a dictionary
+    precipitation_date_tobs = []
+    for each_row in results:
+        dt_dict = {}
+        dt_dict["date"] = each_row.date
+        dt_dict["tobs"] = each_row.tobs
+        precipitation_date_tobs.append(dt_dict)
+
+    return jsonify(precipitation_date_tobs)
+
+
+@app.route("/api/v1.0/stations") #Return a JSON list of stations from the dataset
+def stations():
+    # Create session (link) from Python to the DB
+    session = Session(engine)
+
+    # Query Stations
+    results = session.query(Station.name).all()
+
+    # Convert list of tuples into normal list
+    station_details = list(np.ravel(results))
+
+    return jsonify(station_details)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
